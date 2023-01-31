0d50ff8c8ca9d97552436f424050deb94cacd8a4
Dmytro Yanko <yanko.dmitriy@gmail.com>
2023-01-31 03:41:35 +0200

    image resizer do

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,11 +1,15 @@
 const { User } = require("../../models");
 const fs = require("fs").promises;
+const Jimp = require("jimp");
 const path = require("path");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res, next) => {
   const { path: tempUpload, originalname } = req.file;
+  const image = await Jimp.read(tempUpload);
+  await image.resize(250, 250);
+  await image.writeAsync(tempUpload);
   const { _id: id } = req.user;
   const imageName = `${id}_${originalname}`;
   try {
